6111a4c4291b9188742e6fb97677087d65cdbdfe
Tami <catquarks@gmail.com>
2016-07-27 03:32:34 -0400

    defined Dog and Person classes dogs have names and breeds; people have names and jobs

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Dog
+	def name=(dogs_name)
+		@name = dogs_name
+	end
+
+	def name
+		@name
+	end
+
+	def breed=(dogs_breed)
+		@breed = dogs_breed
+	end
+
+	def breed
+		@breed
+	end
+end
+
+class Person
+	def name=(name)
+		@name = name
+	end
+
+	def name
+		@name
+	end
+
+	def job=(job)
+		@job = job
+	end
+
+	def job
+		@job
+	end
+
+end
